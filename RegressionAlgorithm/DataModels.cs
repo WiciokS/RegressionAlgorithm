8f437128c08ca71a3a05de29ea53e52546375365
WiciokS <77240194+WiciokS@users.noreply.github.com>
2024-02-27 18:12:48 +0200

    Add project files.

---
+++ RegressionAlgorithm/DataModels.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+
+public class HouseData
+{
+    [LoadColumn(0)]
+    public float Size { get; set; }
+
+    [LoadColumn(1)]
+    public float Price { get; set; }
+}
+
+public class Prediction
+{
+    [ColumnName("Score")]
+    public float Price { get; set; }
+}
---
+++ RegressionAlgorithm/Program.cs
@@ -0,0 +1,20 @@
+﻿// Create MLContext
+using Microsoft.ML;
+
+MLContext mlContext = new MLContext();
+
+// Load data
+var trainingData = mlContext.Data.LoadFromTextFile<HouseData>("C:\\Users\\skude\\source\\repos\\RegressionAlgorithm\\RegressionAlgorithm\\house_data.csv", hasHeader: true, separatorChar: ',');
+
+// Data process configuration with pipeline data transformations 
+var pipeline = mlContext.Transforms.Concatenate("Features", new[] { "Size" })
+    .Append(mlContext.Regression.Trainers.Sdca(labelColumnName: "Price", maximumNumberOfIterations: 100));
+
+// Train the model
+var model = pipeline.Fit(trainingData);
+
+// Use the model for a single prediction
+var size = new HouseData() { Size = 750f };
+var price = mlContext.Model.CreatePredictionEngine<HouseData, Prediction>(model).Predict(size);
+
+Console.WriteLine($"Predicted price for size {size.Size}: {price.Price}");
